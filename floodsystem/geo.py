954142377690fb5d3f923a23682e16623dcfdc09
og288 <og288@cam.ac.uk>
2021-01-25 19:07:53 +0000

    Combined og and skc work

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,7 +9,6 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 from haversine import haversine
 
-from haversine import haversine
 
 def stations_by_distance(stations, p):
     '''Lists stations by distance'''
@@ -27,6 +26,19 @@ def stations_by_distance(stations, p):
     return distances
 
 
+def rivers_with_station(stations):
+    """ Given a list of station objects, this function returns a set,
+    with the names of the rivers with a monitoring station.
+    """
+
+    rivers = set()
+
+    for station in stations:
+        rivers.add(station.river)
+
+    return rivers
+
+
 def stations_by_river(stations):
     """ Given a list of station objects, this function returns a dictionary,
     that maps each river with a station on (the keys), to a list of the
@@ -40,19 +52,3 @@ def stations_by_river(stations):
         else:
             dict[station.river] = [station.name]
     return dict
-
-
-def stations_by_distance(stations, p):
-    '''Lists stations by distance'''
-
-    distances = [] # Make an empty list to store the distances from p in
-
-    for station in stations:
-        distance = haversine(station.coord, p) # Calculate distance of p from each station
-        distances.append(distance) # Add this distance to the distances list
-
-    distances = list(zip(stations, distances)) # Create a list of tuples with stations and their distances
-
-    distances = sorted_by_key(distances, 1) # Sort this list
-
-    return distances
