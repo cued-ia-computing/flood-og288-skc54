de19b13bc70261f3fec67c555304882030ece098
og288 <og288@cam.ac.uk>
2021-02-27 17:34:26 +0000

    Task2E stuff

--- Task2E.py
+++ Task2E.py
@@ -1,12 +1,11 @@
-'''This is a preliminary test to see if the pllot_water_levels function for 2E works, it does not fill the objectives listed'''
-""" 
-
-I commented this out to check my stuff worked - Olly
+"""This is a preliminary test to see if the pllot_water_levels function for 2E works,
+it does not fill the objectives listed"""
 
 from floodsystem.plot import plot_water_levels
 from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
-from datetime import datetime, timedelta
+from datetime import datetime  # , timedelta
+
 
 def run():
 
@@ -16,5 +15,5 @@ def run():
     station_levels = fetch_measure_levels(station.id, dt=datetime.timedelta(days=dt))
     plot_water_levels(station, station_levels[0], station_levels[1])
 
+
 run()
-"""
