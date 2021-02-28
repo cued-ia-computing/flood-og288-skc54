5e6fc6747126fff33ef7e73866d7d9b6efa4fde8
Sharafiab <skc54@cam.ac.uk>
2021-02-28 00:30:32 +0000

    Completed task 2E

--- Task2E.py
+++ Task2E.py
@@ -1,19 +1,22 @@
-"""This is a preliminary test to see if the pllot_water_levels function for 2E works,
-it does not fill the objectives listed"""
 
 from floodsystem.plot import plot_water_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
-from datetime import datetime  # , timedelta
+from floodsystem.flood import stations_highest_rel_level
+import datetime
 
 
 def run():
 
-    dt = 10
+    dt = 10 # Data from up to 10 days ago
+
     stations = build_station_list()
-    station = stations[0]
-    station_levels = fetch_measure_levels(station.id, dt=datetime.timedelta(days=dt))
-    plot_water_levels(station, station_levels[0], station_levels[1])
+    update_water_levels(stations)
+    At_risk_stations = stations_highest_rel_level(stations, 5)
+    for i in At_risk_stations: # Create graphs for each station
+        station = i
+        station_levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, station_levels[0], station_levels[1])
 
 
 run()
