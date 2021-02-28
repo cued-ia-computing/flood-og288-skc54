eb70f28507fb8986ac87f7c617ef5b65f6f98cc5
Sharafiab <skc54@cam.ac.uk>
2021-02-28 18:47:38 +0000

    Cosmetic changes

--- Task2G.py
+++ Task2G.py
@@ -7,31 +7,31 @@ import datetime
 def run():
     stations = build_station_list()
     update_water_levels(stations)
-    dt = 2  # Number of days 
+    dt = 2  # Number of days
     nonworking_stations = []  # Create a list for stations with erroneous data
     for i in stations:
         if type(i.relative_water_level()) == float:  # Check to see if the data is corrct and can be used
             station_levels = fetch_measure_levels(i.measure_id, dt=datetime.timedelta(days=dt))
             if len(station_levels[0]) != 0 or len(station_levels[1]) != 0:
-                p = polyfit(station_levels[0], station_levels[1], 1)[0][0] 
+                p = polyfit(station_levels[0], station_levels[1], 1)[0][0]
                 # This takes the coefficient of the polynomial produced by polyfit.
-                # As it is order one, the coefficient is the slope. This can tell us if the water level is rising or decreasing
-                if i.relative_water_level() > 3 and p > 0:  
+                # As it is order one, the coefficient is the slope. This tells us if the water level is rising or not
+                if i.relative_water_level() > 3 and p > 0:
                     # If the relative water level is above 3, the risk is SEVERE
                     print("Risk is SEVERE at", i.name, "and the water level has been RISING")
                 elif i.relative_water_level() > 3 and p < 0:
                     print("Risk is SEVERE at", i.name, "and the water level is DECREASING")
-                elif i.relative_water_level() > 2 and p > 0:  
+                elif i.relative_water_level() > 2 and p > 0:
                     # If the relative water level is above 2, the risk is HIGH
                     print("Risk is HIGH at", i.name, "and the water level is RISING")
-                elif i.relative_water_level() > 2 and p < 0: 
+                elif i.relative_water_level() > 2 and p < 0:
                     print("Risk is HIGH at", i.name, "and the water level is DECREASING")
-                elif i.relative_water_level() > 1 and p > 0:  
+                elif i.relative_water_level() > 1 and p > 0:
                     # If the relative water level is above 1, the risk is MODERATE
                     print("Risk is MODERATE at", i.name, "and the water level is RISING")
                 elif i.relative_water_level() > 1 and p < 0:
                     print("Risk is MODERATE at", i.name, "and the water level is DECREASING")
-                else:  
+                else:
                     # If the relative water level is under 1, the risk is LOW
                     print("Risk is LOW at", i.name)
             else:
@@ -39,6 +39,6 @@ def run():
         else:
             pass
     print("The following stations are returning erroneous data:", nonworking_stations)
-        
-        
+
+
 run()
