7e24ab95023f116affc8ef39596f34b69c0ba266
Sharafiab <skc54@cam.ac.uk>
2021-02-28 17:42:13 +0000

    typo correction

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -7,9 +7,9 @@ import itertools
 def polyfit(dates, levels, p):
     '''Creates a least-squares fit of a polynomial of degree p given time and level data. Returns a tuple of the polynomial and the shift'''
 
-    x = matplotlib.dates.date2num(dates) # the time data is converted to a list fo floats
+    x = matplotlib.dates.date2num(dates) # the time data is converted to a list of floats
     y = levels
-
+    
     p_coeff = np.polyfit(x-x[0], y, p) # time data is shifted so it does not go above 1000, which would cause errors to build up
 
     poly = np.poly1d(p_coeff)
