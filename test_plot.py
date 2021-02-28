f17403223a62726c9a286e0c2c29d3fd4c696e17
Sharafiab <skc54@cam.ac.uk>
2021-02-28 17:43:27 +0000

    Added note about test for plotting functions

---
+++ test_plot.py
@@ -0,0 +1,2 @@
+# There exists in the code for functions plot_water_levels and plot_water_level_with_fit a blacklist for the station 'Hayes Basin. 
+# This is because some of the water level data points are expressed as lists rather than floats. 
