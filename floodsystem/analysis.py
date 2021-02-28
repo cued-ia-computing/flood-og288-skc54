7ed9b74680f1abde154076a53adb4a18cdd2909a
Sharafiab <skc54@cam.ac.uk>
2021-02-28 00:30:10 +0000

    added polyfit function

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,3 +1,23 @@
 '''This module contains functions for analysing data'''
 
-#def polyfit(dates, levels, p):
+import numpy as np
+import matplotlib.dates
+import itertools
+
+def polyfit(dates, levels, p):
+    '''Creates a least-squares fit of a polynomial of degree p given time and level data. Returns a tuple of the polynomial and the shift'''
+
+    x = matplotlib.dates.date2num(dates) # the time data is converted to a list fo floats
+    y = levels
+
+    p_coeff = np.polyfit(x-x[0], y, p) # time data is shifted so it does not go above 1000, which would cause errors to build up
+
+    poly = np.poly1d(p_coeff)
+    
+    d0 = 0
+
+    for i,n in zip(x, y):
+        difference = n - poly(i-x[0])
+        d0 += difference**2
+
+    return (poly, d0)
