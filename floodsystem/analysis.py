8e8e5cd33c967c757b3c6e36c1c4ae5adc4ae6e0
Sharafiab <skc54@cam.ac.uk>
2021-03-01 23:37:17 +0000

    Added checks in function

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -2,22 +2,38 @@
 
 import numpy as np
 import matplotlib.dates
-import itertools
+
 
 def polyfit(dates, levels, p):
-    '''Creates a least-squares fit of a polynomial of degree p given time and level data. Returns a tuple of the polynomial and the shift'''
+    '''Creates a least-squares fit of a polynomial of degree p given time and level data.
+    Returns a tuple of the polynomial and the shift'''
 
-    x = matplotlib.dates.date2num(dates) # the time data is converted to a list of floats
-    y = levels
-    
-    p_coeff = np.polyfit(x-x[0], y, p) # time data is shifted so it does not go above 1000, which would cause errors to build up
+    for i in levels:  # Check that water level data is in the correct form
+        if type(i) == float:
+            pass
+        else:
+            return print("Error with water level data for this station.")
 
-    poly = np.poly1d(p_coeff)
-    
-    d0 = 0
+    if type(dates) == list and type(levels) == list:  # Check the data is given in the right form
+        if len(dates) > 2 and len(levels) > 2:  # Check there are enough data points to work with
+            x = matplotlib.dates.date2num(dates)  # The time data is converted to a list of floats
+            y = levels
+            if type(x) == np.ndarray and type(y) == list:
+                p_coeff = np.polyfit(x - x[0], y, p)
+                # Time data is shifted so it does not go above 1000, which would cause errors to build up
 
-    for i,n in zip(x, y):
-        difference = n - poly(i-x[0])
-        d0 += difference**2
+                poly = np.poly1d(p_coeff)
 
-    return (poly, d0)
+                d0 = 0
+
+                for i, n in zip(x, y):
+                    difference = n - poly(i - x[0])
+                    d0 += difference**2
+
+                return (poly, d0)
+            else:
+                return print("Error with water level data for this station.")  # Error message
+        else:
+            return print("Error with water level data for this station.")
+    else:
+        return print("Error with water level data for this station")
